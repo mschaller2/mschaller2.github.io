10464b869270534ae6f2d382f6a998ffc696e0e2
mschaller2 <37004867+mschaller2@users.noreply.github.com>
2020-04-13 19:39:36 -0500

    real skybox

--- .history/js/map_20200413192108.js
+++ .history/js/map_20200413192108.js
@@ -2,14 +2,20 @@ import * as T from "../CS559-THREE/build/three.module.js";
 import { GrWorld } from "../CS559-Framework/GrWorld.js";
 import { GrObject } from "../CS559-Framework/GrObject.js";
 import * as Helpers from "../CS559-Libs/helpers.js";
-import { CubeTextureLoader } from "../CS559-THREE/build/three.module.js";
-
-
+class customObj extends GrObject{
+  constructor(){
+    let obj = new T.BoxGeometry(2, 2, 2);
+    let box = new T.MeshStandardMaterial({color: "red"});
+    let mesh = new T.Mesh(obj, box);
+    super("Custom", mesh);
+  }
+}
 
 function test() {
   let parentOfCanvas = document.getElementById("div1");
   let world = new GrWorld({ where: parentOfCanvas });
-  world.scene.background = new CubeTextureLoader().load(["../images/singapore.jpg","../images/singapore.jpg","../images/singapore.jpg","../images/singapore.jpg","../images/singapore.jpg","../images/singapore.jpg"]);
+  world.scene.background = new CubeTextureLoader().load(["../images/singapore.jpg"]);
+  world.add(skyBox);
   world.go();
 }
 Helpers.onWindowOnload(test);
